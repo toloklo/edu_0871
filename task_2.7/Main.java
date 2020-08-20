b47fa5146b0b37aeca86ea534321d86bfffe59b1
toloklo <69796972+toloklo@users.noreply.github.com>
2020-08-20 11:20:59 +0300

    Create Main.java

---
+++ task_2.7/Main.java
@@ -0,0 +1,22 @@
+/*
+Напиши код метода sumDigitsInNumber(int number). Метод на вход принимает целое трехзначное число. Нужно посчитать сумму цифр этого числа, и вернуть эту сумму.
+Пример:
+Метод sumDigitsInNumber вызывается с параметром 546.
+Пример вывода:
+15
+*/
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println(sumDigitsInNumber(546));
+    }
+
+    public static int sumDigitsInNumber(int number) {
+        //напишите тут ваш код
+        int sot, des, ed;
+        sot = number/100;
+        des = (number%100)/10;
+        ed = number%10;
+        return (sot+des+ed);
+    }
+}
