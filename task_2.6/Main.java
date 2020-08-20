3c14fc2499e7e244562bcd40b57771d85325bb02
toloklo <69796972+toloklo@users.noreply.github.com>
2020-08-20 11:09:15 +0300

    Create Main.java

---
+++ task_2.6/Main.java
@@ -0,0 +1,19 @@
+/*Выведи на экран таблицу умножения 10 на 10 в следующем виде:
+1 2 3 4 ...
+2 4 6 8 ...
+3 6 9 12 ...
+4 8 12 16 ...
+... */
+
+public class Main {
+    public static void main(String[] args) {
+        //напишите тут ваш код
+        for (int i=1; i<11; i++) {
+            for (int j=1; j<11; j++) {
+                System.out.print(i*j+" ");
+            }
+            System.out.println();
+        }
+
+    }
+}
